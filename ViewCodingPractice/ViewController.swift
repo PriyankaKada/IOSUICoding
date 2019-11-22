103bcbae3ddb2cfe57276f08e92b66e766b8b6fd
DebasishSen <debasishsen@DebasishSens-MacBook-Pro.local>
2019-11-22 12:29:17 +0530

    View UI

--- ViewCodingPractice/ViewController.swift
+++ ViewCodingPractice/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let redView=UIView()
+        redView.backgroundColor = .red
+       
+        
+        
+        
+//        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor,padding: .init(top: 0, left: 16, bottom: 0, right: 16),size: .init(width: 100, height: 100)
+//            )
+      redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor,padding: .init(top: 0, left: 0, bottom: 0, right: 12),size: .init(width: 100, height: 100))
+        
+    
+     view.addSubview(redView)
+    
     }
+    
+    
+}
+extension UIView{
+    func anchor(top:NSLayoutYAxisAnchor?,leading:NSLayoutXAxisAnchor?,bottom:NSLayoutYAxisAnchor?,trailing:NSLayoutXAxisAnchor?, padding:UIEdgeInsets = .zero,size:CGSize = .zero)  {
+        translatesAutoresizingMaskIntoConstraints=false
+        
+        if let top=top{
+            topAnchor.constraint(equalTo: top,constant:padding.top).isActive=true
 
+            
+        }
+        
+        if let leading=leading{
+            leadingAnchor.constraint(equalTo: leading,constant:padding.left).isActive=true
 
+        }
+        
+        if let bottom=bottom{
+        bottomAnchor.constraint(equalTo: bottom,constant:-padding.bottom).isActive=true
+        }
+        
+        if let trailing=trailing{
+        trailingAnchor.constraint(equalTo: trailing,constant:-padding.right).isActive=true
+        }
+        
+    }
 }
 
